aba8987bea9aac6f12c4fa4ad1c57fba7680b6fd
Anders Vikstrand <anders@weasel.fi>
2022-08-30 00:07:54 +0300

    Calendar lookup added

--- components/lookUp.js
+++ components/lookUp.js
@@ -1,9 +1,11 @@
 import topPicks from "./TopPicks/TopPicks";
 import LocalPicks from "./LocalPicks/LocalPicks";
+import Calendar from "./Calendar/Calendar";
 
 const LookUp = {
   "top_picks": topPicks,
-  "local_picks": LocalPicks
+  "local_picks": LocalPicks,
+  "calendar": Calendar
 }
 
 export default LookUp
