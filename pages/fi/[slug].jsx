648d5204d11556a82c85ba3a6215596559b93351
Johannes Lindqvist <johannes@weasel.fi>
2022-11-16 13:09:59 +0200

    Shows blog posts by slug (FINNISH ONLY SO FAR)

---
+++ pages/fi/[slug].jsx
@@ -0,0 +1,40 @@
+import Layout from "../../components/Layout/Layout"
+import IndexPage from "../../pageComponents/Index"
+
+const ContentPost = ({ elements }) => {
+    return (
+        <Layout locale={"fi"}>
+            <IndexPage locale={"fi"} elements={elements} />
+        </Layout>
+
+    )
+}
+
+export const getStaticPaths = async() => {
+
+  // Fetch all pages
+  const pagesAvailable = await fetch(`${process.env.NEXT_PUBLIC_WP_URL}/wp-json/wp/v2/posts?_fields=slug`)
+    .then(res => res.json())
+
+  // Create an array of pages paths
+  const paths = pagesAvailable.map(page => {
+    return { params: { slug: page.slug } }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(process.env.NEXT_PUBLIC_WP_URL + `/wp-json/wp/v2/posts?slug=${params.slug}&acf_format=standard`).then(r => r.json())
+  const elements = res[0]?.acf?.elements
+  return {
+    props: {
+      elements: elements
+    }
+  }
+}
+
+export default ContentPost
