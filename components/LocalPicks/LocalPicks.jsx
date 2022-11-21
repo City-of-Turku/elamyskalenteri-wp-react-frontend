5a6186bd7df7d9f16a720651013802d1859cebaa
essi1 <essi-maria.varjonen@weaselsoftware.fi>
2022-11-21 15:57:54 +0200

    responsive localPicks

--- components/LocalPicks/LocalPicks.jsx
+++ components/LocalPicks/LocalPicks.jsx
@@ -1,11 +1,9 @@
-import { Typography, useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 import React, { useState, useEffect } from 'react';
-import Image from "next/image";
-import vinkLogo from "../../public/svg/vinkLogo1.svg";
 import dayjs from "dayjs";
-import { color } from "@mui/system";
+import styles from "./LocalPicks.module.css";
+import Typography from '@mui/material/Typography';
 
 const LocalPicks = ({ props, locale }) => {
   const [turku, setTurku] = useState(null)
@@ -37,62 +35,50 @@ const LocalPicks = ({ props, locale }) => {
   if (!turku) return <p>No data</p>
   return (
     <>
-      <Box
-        sx={{
-          pb: 4,
-          flexGrow: 1,
-          display: { xs: "none", md: "block" },
-          alignItems: "center",
-          fontSize: 35,
-          fontFamily: "halogen",
-          fontWeight: 900,
-          whiteSpace: "nowrap",
-          wordWrap: "break-word",
-          color: "#193773"
-        }}
-        style={{ textAlign: "center" }}
-      >
-        <span>{props.header[locale]}</span>
-      </Box>
-      <div style={{ display: "flex", justifyContent: "center", gap: 24, flexDirection: "row" }}>
-        {props.column.map((col, index) => (
-          <div key={index} style={{ width: 456, textAlign: "center" }}>
-            <h2 style={{ fontSize: 28, fontFamily: "halogen", backgroundColor: "#9AD9C2", color: "#193773", padding: 16, fontWeight: 900, letterSpacing: 1 }}>{col.content.location}</h2>
-            {props.column.map((item, index) => (
-              <div key={index} className="localpicks">
-                <div style={{ position: "relative" }} >
-                  {turku.images[0]?.url && (
-                    <Box
-                      component="img"
-                      sx={{
-                        height: 105,
-                        width: 165,
-                        borderRadius: 0.8,
-                        boxShadow: 5,
-                        // objectFit: "cover",
-                        maxHeight: { xs: 105, md: 105 },
-                        maxWidth: { xs: 165, md: 165 },
-                      }}
-                      src={turku.images[0]?.url}
-                      alt="Avatar"
-                    />
-                  )}
-                  <div style={{ position: "absolute", left: "30%", bottom: "-43%", transform: "translate(-50%, -50%)", width: 0, height: 0, borderBottom: "100px solid white", borderRight: "100px solid transparent" }}></div>
-                  <Typography sx={{ position: "absolute", left: "14%", bottom: 0, transform: "translate(-50%, -50%)", fontSize: 16, fontWeight: 700, fontStyle: 'normal', letterSpacing: 2, color: '#193773', lineHeight: 2, marginBottom: "10px" }}>
-                    {dayjs(turku.start_time).format("DD")}
-                  </Typography>
-                  <Typography sx={{ position: "absolute", left: "15%", bottom: 0, transform: "translate(-50%, -50%)", fontSize: 12, fontWeight: 500, fontStyle: 'normal', letterSpacing: 2, color: '#193773', lineHeight: 2 }}>
-                    {dayjs(turku.end_time).format("MMM")}
-                  </Typography>
+      <div style={{ justifyContent: "center", margin: "80px 0" }}>
+        <div>
+          <Typography className={styles.localPicksText} style={{ fontFamily: "halogen", color: "#193773" }}>{props.header[locale]}</Typography>
+        </div>
+        <div className={styles.allLocalPicks}>
+          {props.column.map((col, index) => (
+            <div key={index} style={{ textAlign: "center" }}>
+              <h2 style={{ fontSize: 28, fontFamily: "halogen", backgroundColor: "#9AD9C2", color: "#193773", padding: 16, fontWeight: 900, letterSpacing: 1 }}>{col.content.location}</h2>
+              {props.column.map((item, index) => (
+                <div key={index} className={styles.localpicks}>
+                  <div style={{ position: "relative" }} >
+                    {turku.images[0]?.url && (
+                      <Box
+                        component="img"
+                        sx={{
+                          height: 105,
+                          width: 165,
+                          borderRadius: 0.8,
+                          boxShadow: 5,
+                          // objectFit: "cover",
+                          maxHeight: { xs: 105, md: 105 },
+                          maxWidth: { xs: 165, md: 165 },
+                        }}
+                        src={turku.images[0]?.url}
+                        alt="Avatar"
+                      />
+                    )}
+                    <div style={{ position: "absolute", left: "30%", bottom: "-43%", transform: "translate(-50%, -50%)", width: 0, height: 0, borderBottom: "100px solid white", borderRight: "100px solid transparent" }}></div>
+                    <Typography sx={{ position: "absolute", left: "14%", bottom: 0, transform: "translate(-50%, -50%)", fontSize: 16, fontWeight: 700, fontStyle: 'normal', letterSpacing: 2, color: '#193773', lineHeight: 2, marginBottom: "10px" }}>
+                      {dayjs(turku.start_time).format("DD")}
+                    </Typography>
+                    <Typography sx={{ position: "absolute", left: "15%", bottom: 0, transform: "translate(-50%, -50%)", fontSize: 12, fontWeight: 500, fontStyle: 'normal', letterSpacing: 2, color: '#193773', lineHeight: 2 }}>
+                      {dayjs(turku.end_time).format("MMM")}
+                    </Typography>
+                  </div>
+                  <div style={{ textAlign: "left", paddingLeft: 25 }}>
+                    <Typography sx={{ fontSize: 14, fontFamily: 'Forma DJR Micro', fontWeight: 500, fontStyle: 'normal', letterSpacing: 2, color: '#193773', lineHeight: 2 }}>{dayjs(turku.start_time).format("DD.MM.YYYY | HH:mm")} - {dayjs(turku.end_time).format("HH:mm")}</Typography>
+                    <Typography style={{ fontSize: 18, fontWeight: 900, color: "#F26835", fontFamily: "halogen" }} key={item.id}>{turku.name.fi}</Typography>
+                  </div>
                 </div>
-                <div style={{ textAlign: "left", paddingLeft: 25 }}>
-                  <Typography sx={{ fontSize: 14, fontFamily: 'Forma DJR Micro', fontWeight: 500, fontStyle: 'normal', letterSpacing: 2, color: '#193773', lineHeight: 2 }}>{dayjs(turku.start_time).format("DD.MM.YYYY | HH:mm")} - {dayjs(turku.end_time).format("HH:mm")}</Typography>
-                  <Typography style={{ fontSize: 18, fontWeight: 900, color: "#F26835", fontFamily: "halogen" }} key={item.id}>{turku.name.fi}</Typography>
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
